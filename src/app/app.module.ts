7c4c8982fbd96dc8ab611a96026e5f90e8f6c301
Anksoni02 <anksoni0205@gmail.com>
2022-11-24 15:56:52 +0530

    Forms added for MDO
    Forms added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OptionsComponent } from './options/options.component';
+import { AddformComponent } from './addform/addform.component';
+import { UpdateformComponent } from './updateform/updateform.component';
+import { DeleteformComponent } from './deleteform/deleteform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    OptionsComponent,
+    AddformComponent,
+    UpdateformComponent,
+    DeleteformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
